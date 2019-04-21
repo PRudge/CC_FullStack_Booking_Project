cbf5681449a42b82dae16bacb4668792345e1f2b
Ruben Franco <rubenfrancosanchez@hotmail.es>
2019-04-21 14:26:19 +0100

    Resertavion Projection

--- backend/reservations/src/main/java/com/codeclan/reservations/reservations/projections/ReservationProjection.java
+++ backend/reservations/src/main/java/com/codeclan/reservations/reservations/projections/ReservationProjection.java
@@ -1,4 +1,15 @@
 package com.codeclan.reservations.reservations.projections;
 
-public class ReservationProjection {
+import com.codeclan.reservations.reservations.models.Customer;
+import com.codeclan.reservations.reservations.models.Reservation;
+import com.codeclan.reservations.reservations.models.Restaurant;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name= "embedRestaurant", types = Reservation.class)
+public interface ReservationProjection {
+    long getId();
+    String getDate();
+    String getStartTime();
+    Restaurant getRestauran();
+    Customer getCustomer();
 }
