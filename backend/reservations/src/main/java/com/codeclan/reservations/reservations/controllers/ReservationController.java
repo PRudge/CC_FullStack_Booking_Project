c495aa9df01c579f60fbc424dfe033a6ca0f8f45
Pauline Rudge <paulinerudge@me.com>
2019-04-21 13:16:58 +0100

    added reservations and reservations by date to reservation controller

--- backend/reservations/src/main/java/com/codeclan/reservations/reservations/controllers/ReservationController.java
+++ backend/reservations/src/main/java/com/codeclan/reservations/reservations/controllers/ReservationController.java
@@ -1,4 +1,37 @@
 package com.codeclan.reservations.reservations.controllers;
 
+import com.codeclan.reservations.reservations.models.Reservation;
+import com.codeclan.reservations.reservations.repository.reservations.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
+    @Autowired
+    ReservationRepository reservationRepository;
+
+
+//    List<Reservation> findReservationsByDate(String date);
+//    List<Reservation> findReservationsForAGivenCustomer(Long customerId);
+//    List <Reservation> findReservationsForAGivenDateForAGivenTime(String date, String startTime);
+
+
+
+    @GetMapping("/{date}")
+    public List<Reservation> getFindReservationsByDate(@PathVariable String date){
+        return reservationRepository.findReservationsByDate(date);
+    }
+
 }
+
