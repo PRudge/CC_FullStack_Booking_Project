4bbd4a7bc49285049ac2bb28e8812a1f5bb84b59
Nuno Farinha <nadfarinha@gmail.com>
2019-04-21 13:33:30 +0100

    Add updated code to app component

--- frontend/reservations/src/App.js
+++ frontend/reservations/src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './NavBar';
 import ReservationListContainer from './containers/reservations/ReservationListContainer';
+import ReservationFormContainer from './containers/reservations/ReservationFormContainer';
 
 class App extends Component {
-  render() {
-    return (
-      <Router >
-        <React.Fragment>
-          <NavBar />
-          <ReservationListContainer />
-        </React.Fragment>
-      </Router>
-    );
-  }
+ render() {
+   return (
+     <Router >
+       <React.Fragment>
+       <NavBar />
+
+           <Route exact path = '/reservations' component={ReservationListContainer}/>
+           <Route exact path = '/reservations/new' component= {ReservationFormContainer} />
+
+       </React.Fragment>
+     </Router>
+   );
+ }
 }
 
 export default App;
