57753bcd4ff02ae90deaf8b3eaa5445bfec9805f
Abigaila Ekierte <arekiert@gmail.com>
2019-04-22 15:19:26 +0100

    routes fixed

--- frontend/reservations/src/App.js
+++ frontend/reservations/src/App.js
@@ -4,6 +4,7 @@ import Request from './helpers/Request.js';
 import NavBar from './NavBar';
 import ReservationList from './components/reservations/ReservationList';
 import ReservationFormContainer from './containers/reservations/ReservationFormContainer';
+import ErrorPage from './components/reservations/ErrorPage';
 
 
 
@@ -21,20 +22,21 @@ class App extends Component {
   }
 
  render() {
-   console.log('app.js: ', this.state.reservations);
    return (
      <Router >
        <React.Fragment>
-       <NavBar />
+        <NavBar />
+          <Switch>
            <Route
-            path = '/reservations'
+            exact path = '/reservations'
             render={() => <ReservationList reservations={this.state.reservations}/>}
            />
            <Route
            path = '/reservations/new'
            render={ () => <ReservationFormContainer reservations={this.state.reservations} /> }
            />
-
+           <Route component={ErrorPage} />
+          </Switch>
        </React.Fragment>
      </Router>
    );
