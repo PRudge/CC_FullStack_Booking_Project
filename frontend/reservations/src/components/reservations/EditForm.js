3514edcffc1ab84dc0fe41dc8c44872422cb9f37
Nuno Farinha <nadfarinha@gmail.com>
2019-04-22 16:00:23 +0100

    Create reservation edit form

---
+++ frontend/reservations/src/components/reservations/EditForm.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+
+class EditForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime: props.reservation.startTime,
+      date: props.reservation.date,
+      numGuest: props.reservation.numGuest,
+      customer: props.reservation._links.self.href.replace("{?projection}", "")
+    }
+    console.log("EditForm after constructor: ", this.state);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(props) {
+    console.log('EditForm will receive props',props);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('Edit Form handle submit: ',this.state);
+    const reservation = {
+      "startTime": this.state.startTime,
+      "date": this.state.date,
+      "numGuest": this.state.numGuest,
+      "customer": this.state.customer
+    }
+    this.props.handleReservationEdit(reservation);
+  }
+
+  render(){
+
+    const customerOptions = this.props.customer.map((customerItem, index) => {
+      return <option key={index} value={customerItem._links.self.href}>{customerItem.name}</option>
+    })
+
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value = {this.state.startTime} name="startTime" onChange={e => this.setState({ startTime: e.target.value })}/>
+          <input type="text" value = {this.state.date} name="date" onChange={e => this.setState({ date: e.target.value })}/>
+          <input type="number" value = {this.state.numGuest} name="numGuest" onChange={e => this.setState({ numGuest: e.target.value })}/>
+
+
+          <select name="customer" onChange={e => this.setState({ customer: e.target.value })}>
+            {customerOptions}
+          </select>
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    )
+  }
+
+
+}
+
+export default EditForm;
--- frontend/reservations/src/containers/reservations/EditFormContainer.js
+++ frontend/reservations/src/containers/reservations/EditFormContainer.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
+import EditForm from '../../components/reservations/EditForm';
+import Request from '../../helpers/Request';
 
 class EditFormContainer extends Component {
-  // constructor(props) {
-  //   this.state = {
-  //     "date":,
-  //     "startTime":,
-  //     "numGuest":
-  //   }
-  // }
-  render() {
-    return <h1>Edit Form</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      reservation: null,
+      customer: null
+    }
+    this.handleReservationEdit = this.handleReservationEdit.bind(this);
+  }
+
+  componentDidMount(){
+    const request = new Request();
+    request.get("/reservations/" + this.props.id + "?projection=embedCustomer")
+      .then((reservation) => {
+        this.setState({reservation: reservation})
+      });
+    request.get("/customers").then((customer) => {
+      this.setState({customer: customer._embedded.customers})
+    });
+  }
+
+  handleReservationEdit(reservation){
+    const request = new Request();
+    request.patch('/reservations/' + this.props.id, reservation).then(() => {
+      window.location = '/reservations/' + this.props.id
+    })
+  }
+  render(){
+    if(!this.state.customer || !this.state.reservation){
+      return <h1>loading</h1>;
+    }
+    return <EditForm customer = {this.state.customer} reservation={this.state.reservation} handleReservationEdit= {this.handleReservationEdit} />
+
   }
 
 }
