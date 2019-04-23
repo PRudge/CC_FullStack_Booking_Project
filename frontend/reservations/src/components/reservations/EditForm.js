0976f8983a065366da308409a2aec08567fb8b95
Nuno Farinha <nadfarinha@gmail.com>
2019-04-23 09:30:49 +0100

    fix customer state

--- frontend/reservations/src/components/reservations/EditForm.js
+++ frontend/reservations/src/components/reservations/EditForm.js
@@ -7,7 +7,8 @@ class EditForm extends Component {
       startTime: props.reservation.startTime,
       date: props.reservation.date,
       numGuest: props.reservation.numGuest,
-      customer: props.reservation._links.self.href.replace("{?projection}", "")
+      // customer: props.reservation._links.self.href.replace("{?projection=embedCustomer}", "")
+      customer: props.customer
     }
     console.log("EditForm after constructor: ", this.state);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -35,13 +36,13 @@ class EditForm extends Component {
       return <option key={index} value={customerItem._links.self.href}>{customerItem.name}</option>
     })
 
-
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <input type="text" value = {this.state.startTime} name="startTime" onChange={e => this.setState({ startTime: e.target.value })}/>
           <input type="text" value = {this.state.date} name="date" onChange={e => this.setState({ date: e.target.value })}/>
           <input type="number" value = {this.state.numGuest} name="numGuest" onChange={e => this.setState({ numGuest: e.target.value })}/>
+          {/* <input type = "text" value = {this.state.customer.firstName} name="firstName" onChange={e => this.setState({ customer.firstName: e.target.value })} /> */}
 
 
           <select name="customer" onChange={e => this.setState({ customer: e.target.value })}>
