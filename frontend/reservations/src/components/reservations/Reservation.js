1fc739aa9bdc6c02fd3f610d9cc0428ea7ec8a1e
Abigaila Ekierte <arekiert@gmail.com>
2019-04-23 15:11:47 +0100

    fixed table

--- frontend/reservations/src/components/reservations/Reservation.js
+++ frontend/reservations/src/components/reservations/Reservation.js
@@ -6,23 +6,21 @@ const Reservation = (props) => {
   if(!props.date && !props.startTime && !props.numGuest) { return null; }
 
   return (
-    <div>
         <tr>
-          <td>{props.firstName} {props.lastName}</td>
-          <td>{props.date}</td>
-          <td>{props.startTime}</td>
-          <td>{props.numGuest}</td>
-          <td>{props.phoneNum}</td>
-          <td>
-            <form method="GET">
-              <input type="submit" value="edit" />
-            </form>
-            <form action="/products/<%= product.id %>/delete" method="POST">
-              <input type="submit" value="delete" />
-            </form>
-          </td>
+            <td>{props.firstName} {props.lastName}</td>
+            <td>{props.date}</td>
+            <td>{props.startTime}</td>
+            <td>{props.numGuest}</td>
+            <td>{props.phoneNum}</td>
+            <td>
+              <form method="GET">
+                <input type="submit" value="edit" />
+              </form>
+              <form method="POST">
+                <input type="submit" value="delete" />
+              </form>
+            </td>
         </tr>
-    </div>
   )
 
 
--- frontend/reservations/src/components/reservations/ReservationList.js
+++ frontend/reservations/src/components/reservations/ReservationList.js
@@ -4,27 +4,26 @@ import Reservation from './Reservation';
 const ReservationList = ({reservations}) => {
     const allReservations = reservations.map((reservation) => {
       return (
-        <div key={reservation.id} className="reservation-item">
-          <div className="reservation">
+        <tbody key={reservation.id} className="reservation">
             <Reservation firstName={reservation.customer.firstName} lastName={reservation.customer.lastName} date={reservation.date} startTime={reservation.startTime} numGuest={reservation.numGuest} phoneNum={reservation.customer.phoneNum}/>
-          </div>
-        </div>
+        </tbody>
       );
     });
 
     return (
         <div className='reservations-container'>
           <table className="table">
-            <tr>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Start time</th>
-              <th>Total Guests</th>
-              <th>Phone Number</th>
-              <th></th>
-            </tr>
-
-            {allReservations}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Start time</th>
+                <th>Total Guests</th>
+                <th>Phone Number</th>
+                <th></th>
+              </tr>
+            </thead>
+                {allReservations} 
           </table>
         </div>
 
