8e822be6e1faebb0a5da66017a2bf39b50708e4a
Pauline Rudge <paulinerudge@me.com>
2019-04-23 21:22:52 +0100

    adding form css

--- frontend/reservations/src/NavBar.js
+++ frontend/reservations/src/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import ReservationTable from './css/ReservationsTable.css';
+
+
 const NavBar = (props) => {
   return (
     <header>
-      <img src="/images/calendar.png" alt="Logo"/>
-
+      <img src="/images/calendar.jpg" alt="Logo"/>
+      <nav>
       <ul>
         <li className="navLink">
           <Link to="/reservations">Reservations</Link>
@@ -15,6 +18,7 @@ const NavBar = (props) => {
         </li>
 
       </ul>
+      </nav>
     </header>
   )
 }
